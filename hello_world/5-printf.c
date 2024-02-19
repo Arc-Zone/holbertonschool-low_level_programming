3f09a3db7748a58dccc6245e332bf6e467409a17
Zone <enzo.gomez@3wa.io>
2024-02-19 11:35:07 +0000

    fixe 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 /**
- * function main print with prinf
- */ 
+ * main - Entry point
+ *
+ * Description: Print a message using printf
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
