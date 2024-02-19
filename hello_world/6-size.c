dd1c80bd64cba828646d82de640fae7b3b6cc821
Zone <enzo.gomez@3wa.io>
2024-02-19 12:32:28 +0000

    fix 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeOf(char));
-	printf("Size of a char: %lu byte(s)\n", sizeOf(int));
-	printf("Size of a char: %lu byte(s)\n", sizeOf(long int));
-	printf("Size of a char: %lu byte(s)\n", sizeOf(long long int));
-	printf("Size of a char: %lu byte(s)\n", sizeOf(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", sizeof(int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(float));
 
 	return(0);
 }
