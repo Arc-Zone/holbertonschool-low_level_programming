23bf49593e92c39e27fa7a8736a3ba54bcab414e
Zone <enzo.gomez@3wa.io>
2024-02-19 10:40:27 +0000

    add  4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-/*
+/**
  *fonction main renvoie une string et retourne 
  */
+
+//fonction main
 int main(void) {
 puts ("\"Programming is like building a multilingual puzzle");	
 return 0;
