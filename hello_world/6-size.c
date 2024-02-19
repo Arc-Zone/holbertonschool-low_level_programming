d0c2ce4d9734d9ac7e9c93a36f5066edfa1187c0
Zone <enzo.gomez@3wa.io>
2024-02-19 13:17:12 +0000

    betty edit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-/*
- * Write a C program that prints the size of various types on the computer it is compiled and run on.
+/**
+ * main - Entry point 
+ *
+ * Description : print size 
+ *
+ * Return: Always 0 (Success)
+ * 
  */
 
 int main(void)
