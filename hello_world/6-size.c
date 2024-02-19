7db95553094f302bc495024f6dcee1dea547401a
Zone <enzo.gomez@3wa.io>
2024-02-19 12:48:07 +0000

    -m64

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
  * Write a C program that prints the size of various types on the computer it is compiled and run on.
  */
 
-int main(void)
+int main (void)
 {
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of a char: %lu byte(s)\n",  (unsigned long)sizeof(int));
@@ -12,5 +12,5 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n",  (unsigned long)sizeof(long long int));
 	printf("Size of a char: %lu byte(s)\n",  (unsigned long)sizeof(float));
 
-	return(0);
+	return (0);
 }
