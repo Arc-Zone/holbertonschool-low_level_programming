c4a27388115dd8749af8a20152f957341555d08e
Zone <enzo.gomez@3wa.io>
2024-02-20 13:21:02 +0000

    add 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : print size
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i ;
+	for(i = 0; i<10; i++)
+	{
+		putchar('0'+ i);
+	}
+
+	char a = 'a';
+	while(a < 'f'){
+		putchar(a);
+		a++;
+	}
+	putchar('\n');	
+}
