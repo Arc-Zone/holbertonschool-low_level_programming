6d85e05b628c36a16153be2510de2f09431825ab
Zone <enzo.gomez@3wa.io>
2024-02-20 13:40:26 +0000

    fixe 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -12,12 +12,12 @@
 int main(void)
 {
 	int i;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10 ; i++)
 	{
 		putchar('0' + i);
 	}
 
-	for (i = 'a' ; i <= 'f' ;i++)
+	for (i = 'a' ; i <= 'f' ; i++)
 	{
 		putchar(i);
 	}
