fb1a31dca63039c5ec9e6ead3e04eddd3e6d6b20
Zone <enzo.gomez@3wa.io>
2024-02-20 12:15:46 +0000

    fix  6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-int main ()
+/**
+ * main - Entry point
+ *
+ * Description : print size
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int i = 0;
-	while(i < 10)
+
+	while (i < 10)
 	{
 		putchar('0' + i);
 		i++;
 	}
 	putchar('\n');
-	return (0);	
-}	
+	return (0);
+}
