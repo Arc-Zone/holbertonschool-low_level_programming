f40bb35962011a8e39b50bc4ae5f39db72fb6d9a
Zone <enzo.gomez@3wa.io>
2024-02-20 10:22:41 +0000

    fix 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : print size
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if(alphabet == 'e' && alphabet == 'q' ) 
+		{
+			putchar("");
+		}else {
+		putchar(alphabet);
+		
+		}
+		alphabet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
