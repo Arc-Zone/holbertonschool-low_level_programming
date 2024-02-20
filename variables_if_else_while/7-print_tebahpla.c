f5f5b401cd8b73b0b40f8faee22c574cbcc2b404
Zone <enzo.gomez@3wa.io>
2024-02-20 12:20:42 +0000

    add 7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : print size
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'z'
+	
+	while(alphabet => 'a'){
+	putchar(alphabet)
+	}
+	alphabet--
+	return (0)
+}
