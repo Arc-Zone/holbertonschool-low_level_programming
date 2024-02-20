7ad82f65fa81f8db5e28c39f67841ed22801a708
Zone <enzo.gomez@3wa.io>
2024-02-20 09:59:06 +0000

    fix betty style

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : print size
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alphabet = 'a';
