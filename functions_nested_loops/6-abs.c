dee901ec1e84b8e1f712bea7c585c3c23344a27d
Zone <enzo.gomez@3wa.io>
2024-02-21 13:42:49 +0000

    add 6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+/**
+ * _abs  - Entry point
+ *
+ * Description : print size
+ *
+ * Return: Always 0 (Success)
+ */
 int _abs(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
-		return -n;
-	}{
-		return n;
+		return (-n);
+	} {
+		return (n);
 	}
 
 }
