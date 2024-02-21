c68fe97aefe74610ea81da9626c3e0fc9328d915
Zone <enzo.gomez@3wa.io>
2024-02-21 09:37:32 +0000

    add 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,6 +1,27 @@
-#include "main.h"
-int main (void)
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
 {
-	print_alphabet();
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'e' &&  alphabet != 'q')
+		{
+			putchar(alphabet);
+		}
+		alphabet++;
+	}
+	putchar('\n');
+
 	return (0);
 }
