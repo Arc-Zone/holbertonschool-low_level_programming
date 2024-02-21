16446df2b2591666ffce8ef3aaf1349de72613c1
Zone <enzo.gomez@3wa.io>
2024-02-21 10:22:13 +0000

    fix print_alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,16 @@
 void print_alphabet_x10(void)
 {
 	char alphabet = 'a';
-	int i;
-	for(i = 0 ; i < 11 ; i++)
-	{
 
-	while (alphabet <= 'z')
+	int i;
+
+	for (i = 0 ; i < 11 ; i++)
 	{
+		while (alphabet <= 'z')
+		{
 		_putchar(alphabet);
 		alphabet++;
-	}
+		}
 	_putchar('\n');
 	}
 }
