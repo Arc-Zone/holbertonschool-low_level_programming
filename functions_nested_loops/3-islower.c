0fdbdaae67f3a7b4a3072a010c16d75f0c98c707
Zone <enzo.gomez@3wa.io>
2024-02-21 11:04:15 +0000

    add 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * _islower - entry point
+ *
+ * @c: this character is in ASCII
+ *
+ * Description: checks for lowercase character.
+ *
+ * Return: 0 for success.
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
