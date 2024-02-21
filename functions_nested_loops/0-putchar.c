07004da4a697d6bfadf35b3fb2b70646c6bd5501
Zone <enzo.gomez@3wa.io>
2024-02-21 09:22:08 +0000

    fix 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        __putchar(_);
-        __putchar(_);
-        __putchar(p);
-        __putchar(u);
-        __putchar(t);
-        __putchar(c);
-        __putchar(h);
-        __putchar(a);
-        __putchar(r);
-        return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
