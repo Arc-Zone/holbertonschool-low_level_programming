bb23fc8bd0b643547581b08927b1288a5ddabb2f
Zone <enzo.gomez@3wa.io>
2024-02-21 10:14:14 +0000

    fezdsqziufnjs

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet  - Entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Description : print size
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
