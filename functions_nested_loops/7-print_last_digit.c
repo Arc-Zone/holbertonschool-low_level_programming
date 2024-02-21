f2f7b9f0cc127f1460f6eee1062298dd9769e009
Zone <enzo.gomez@3wa.io>
2024-02-21 14:38:23 +0000

    add 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * print_last_digit - entry point
+ *
+ * @n: number
+ *
+ * Description: prints the last digit of a number
+ *
+ * Return: 0 for success.
+ */
 
 int print_last_digit(int n)
 {
