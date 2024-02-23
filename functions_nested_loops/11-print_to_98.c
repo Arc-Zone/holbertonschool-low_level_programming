0cf8bbb5be9915ebfa4f88619f0ad282d5e7ee5c
Zone <enzo.gomez@3wa.io>
2024-02-23 10:09:58 +0000

    add 11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void print_to_98(int n)
+{
+	if( n < 98 )
+	{
+		for(n ; n  < 98 ; i++)
+		{
+			_putchar(n);
+		}
+			
+	}else if (n > 98)
+		for(n ; n  > 98 ; i--){
+			_putchar(n);
+		}
+}
