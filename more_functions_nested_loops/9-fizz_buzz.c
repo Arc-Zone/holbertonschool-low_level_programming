be8f0eeeb1c102069e6d8d9cc3d95045d8698cd9
Zone <enzogomez199933@gmail.com>
2024-02-26 14:41:23 +0000

    add add 8-print_square.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int main (int c )
+{
+	int i ;
+	
+	for(i = 0 ; i <= 100 ; i++)
+	{
+		if(i % 3 === 0){
+			_putchar('Fizz');
+		} else if (i % 5 === 0)
+		{
+			_putchar('Buzz');
+		} {
+			 _putchar('FizzBuzz');
+		}			
+		_putchar('0' + i);
+	}		
+_putchar('\n')
+}
