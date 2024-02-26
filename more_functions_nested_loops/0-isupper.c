6b29701c20714ded9a87eabc3bf5096238025444
Zone <enzogomez199933@gmail.com>
2024-02-26 09:11:56 +0000

    add 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,12 @@
+/**
+ * _isupper  - entry point
+ *
+ * @c: number entry
+ *
+ * Description: check in ASCII value if is upper or not 
+ *
+ * Return: 1 for success and 0 for failed .
+ */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
