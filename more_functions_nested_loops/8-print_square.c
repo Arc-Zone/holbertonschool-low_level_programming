d4a7f9f55131566817d06d82904fa96c8e0942e4
Zone <enzogomez199933@gmail.com>
2024-02-26 14:21:30 +0000

    add 8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,22 +2,19 @@
 void print_square(int size)
 {
 	int i, j;
-	
-	   if (size != 0)
-	   {
-		   putchar('\n');
-	   }
-	for (i = 0 ; i <= size ; i++)
-	{
-	
-			_putchar('#');
+
+	if (size > 0)
+	{  
+		for (i = 0 ; i <= size ; i++)
+		{
 				for (j = 0 ; j <= size ; j++)
 				{
 		 		_putchar('#');	
 				}
 		
-		 _putchar('\n');
+			 _putchar('\n');
 
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
