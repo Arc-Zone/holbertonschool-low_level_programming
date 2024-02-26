67b7314b6dd8060135a7895beceebbc8f27fd262
Zone <enzogomez199933@gmail.com>
2024-02-26 10:44:12 +0000

    add 5-more-numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,11 +5,14 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0 ; i < 15 ; i++)
+	for (i = 0 ; i < 11 ; i++)
 	{
 		for (j = 0 ; j < 15 ; j++)
 		{
+			if (j < 10 ){	
 			_putchar('0' + j);
+			}
+		_puthcar((i % 10) + '0');
 		}	
 	_putchar('\n');
 	}
