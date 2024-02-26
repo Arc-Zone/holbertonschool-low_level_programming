a1af335a2ce9a4acb7710ed63adffcccf8e3e82c
Zone <enzogomez199933@gmail.com>
2024-02-26 09:26:08 +0000

    add 1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,3 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - entry point
+ *
+ * @c: number
+ *
+ * Description: prints 1 if c <= 9
+ *
+ * Return: 1 for success return 0 if failed.
+ */
+
 int _isdigit(int c)
 {
 	if(c <= 9 && c >= 0)
