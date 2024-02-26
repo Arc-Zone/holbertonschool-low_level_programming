38b2833845b7e006c437f4b5cb79a3eac08d9535
Zone <enzogomez199933@gmail.com>
2024-02-26 12:27:30 +0000

    add  6-print_line.

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
+/**
+ * print_line  - entry point
+ *
+ * Description: return a *b result
+ *
+ * Return: 0 to 9
+ */
 void print_line(int n)
 {
-        int i;
-        for (i = 0 ; i < n ; i++)
-        {
-                _putchar('_');
-        }
-        _putchar('\n');
+	int i;
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
