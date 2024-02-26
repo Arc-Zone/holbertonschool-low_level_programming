2d71287d201750a3c2601bef194cd97f8a9e5c38
Zone <enzogomez199933@gmail.com>
2024-02-26 12:39:06 +0000

    add 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,10 +3,14 @@
 void print_diagonal(int n)
 {
 
-	int i;
+	int i , j;
 
 	for (i = 0 ; i < n ; i++)
 	{
+		for (j = 0 ; j < i ; i++)
+		{
+			_putchar(" ")
+		}
 		_putchar('\\');
 	}
 	_putchar('\n');
