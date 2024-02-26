d8e009305da593da0a501d13093079e505afcffe
Zone <enzogomez199933@gmail.com>
2024-02-26 14:32:52 +0000

    add add 8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,32 @@
 #include "main.h"
+
+/**
+ * print_square - entry poinit
+ *
+ * @size: number
+ *
+ * Description: prints a square, followed by a new line.
+ *
+ * Return: 0 for success.
+ */
+
 void print_square(int size)
 {
-	int i, j;
+	int count;
+	int space;
 
 	if (size > 0)
-	{  
-		for (i = 0 ; i <= size ; i++)
+	{
+		for (count = 0; count < size; count++)
 		{
-			for (j = 0 ; j < size ; j++)
+			for (space = 0; space < size; space++)
 			{
-		 		_putchar('#');	
+				_putchar('#');
 			}
-				
-		 _putchar('\n');
-
-		} 
-	} {
-	 _putchar('\n');
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
 	}
 }
