21c8081900cc16d5be3737cbe02a9717b16b0190
Zone <enzogomez199933@gmail.com>
2024-02-26 09:33:36 +0000

    add 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,6 +1,15 @@
 #include "main.h"
-
+/**
+ * mul  - entry point
+ *
+ * @a: number
+ * &b: number 
+ *
+ * Description: return a *b result
+ *
+ * Return: a * b result 
+ */
 int mul(int a, int b)
 {
-	return (a * b) ;
+	return (a * b);
 }
